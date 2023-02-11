6aa27620ba52743f33b2231967b2f8d17c40026e
Nailya <n.murt@yandex.ru>
2023-02-11 23:10:18 +0300

    Task 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,4 +2,26 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine("Hello, World!");
+using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Сколько чисел вводить: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Выводим массив: [{0}]", string.Join(", ", NCount(n))); ;
+    }
+
+    static int[] NCount(int n)
+    {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Ввидите число: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return array;
+    }
+
+}
