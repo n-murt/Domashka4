d3fb8107eb57b75eaf0b6fd81ac172d107409db0
Nailya <n.murt@yandex.ru>
2023-02-11 23:08:53 +0300

    Task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,3 +2,28 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите A: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите B: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"A в степени B - {Program.Exponentiation(a, b)}");
+    }
+
+    static int Exponentiation(int a, int b)
+    {
+        int c = 1;
+        for (int i = 0; i < b; i++)
+        {
+            c = c * a;
+        }
+        return c;
+    }
+
+}
